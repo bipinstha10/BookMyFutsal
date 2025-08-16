be84ac8c52b5143a724bef6c5c0181144419de7a
Bipin <beepeenstha10@gmail.com>
2025-08-16 13:19:56 +0545

    refactor: remove static data file

--- app/src/data/auth.ts
+++
@@ -1,6 +0,0 @@
-import type { User } from "./users";
-import { rolePermissions } from "./permissions";
-
-export function can(user: User, action: string): boolean {
-  return rolePermissions[user.role]?.includes(action) ?? false;
-}
