1df142b1bcf8ee320d7d4d6352b6aeabe4d33bf9
Bipin <beepeenstha10@gmail.com>
2025-12-14 21:42:51 +0545

    chore: update port

--- app/src/redux/api/base-api.ts
+++ app/src/redux/api/base-api.ts
@@ -2,7 +2,7 @@ import { retry } from "@reduxjs/toolkit/query";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
-  baseQuery: retry(fetchBaseQuery({ baseUrl: "http://localhost:4001" }), {
+  baseQuery: retry(fetchBaseQuery({ baseUrl: "http://localhost:3000" }), {
     maxRetries: 3,
   }),
   tagTypes: ["futsals", "futsal", "slots", "bookings", "users", "user"],
