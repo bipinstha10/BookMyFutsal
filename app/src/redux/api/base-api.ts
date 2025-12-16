636e32910a5c37e6262e9520c60888edd7d527fb
Bipin <beepeenstha10@gmail.com>
2025-12-16 21:58:28 +0545

    add credential paramter to set cookies

--- app/src/redux/api/base-api.ts
+++ app/src/redux/api/base-api.ts
@@ -2,9 +2,15 @@ import { retry } from "@reduxjs/toolkit/query";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
-  baseQuery: retry(fetchBaseQuery({ baseUrl: "http://localhost:3000" }), {
-    maxRetries: 3,
-  }),
+  baseQuery: retry(
+    fetchBaseQuery({
+      baseUrl: "http://localhost:3000",
+      credentials: "include",
+    }),
+    {
+      maxRetries: 3,
+    }
+  ),
   tagTypes: ["futsals", "futsal", "slots", "bookings", "users", "user"],
   endpoints: () => ({}),
 });
