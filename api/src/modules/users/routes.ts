01824e0131b3df3402be897745f06c8c664f0677
Bipin <beepeenstha10@gmail.com>
2025-12-16 22:03:51 +0545

    feat: add logout route

--- api/src/modules/users/routes.ts
+++ api/src/modules/users/routes.ts
@@ -1,10 +1,17 @@
 import type { FastifyInstance } from "fastify";
-import { loginHandler, registerUserHandler } from "./controllers";
+import {
+  loginHandler,
+  logoutHandler,
+  registerUserHandler,
+} from "./controllers";
+import { verifyJWT } from "./hooks";
 
 async function userRoutes(server: FastifyInstance) {
   server.post("/", registerUserHandler);
 
   server.post("/login", loginHandler);
+
+  server.post("/logout", { preHandler: [verifyJWT] }, logoutHandler);
 }
 
 export default userRoutes;
